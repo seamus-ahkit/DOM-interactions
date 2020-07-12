2c4d39d29e680d22b4de06d8d77d887d0efbc89c
seamus-ahkit <seamusae@gmail.com>
2020-07-12 16:42:34 +1200

    Complete DOM interactions challenge

--- game.js
+++ game.js
@@ -12,6 +12,8 @@ function bindEventListeners (dots) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', hide)
   }
 }
 
@@ -23,7 +25,17 @@ function makeGreen (evt) {
 
 // CREATE FUNCTION makeBlue HERE
 
+function makeBlue (evt) {
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
+
 // CREATE FUNCTION hide HERE
+
+function hide (evt) {
+  evt.target.classList.toggle('invisible')
+  updateCounts()
+}
 
 function updateCounts () {
   var totals = {
@@ -33,6 +45,15 @@ function updateCounts () {
   }
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+
+  var greenCount = document.getElementsByClassName("green").length;
+  totals.green = greenCount
+
+  var blueCount = document.getElementsByClassName("blue").length;
+  totals.blue = blueCount
+
+  var invisibleCount = document.getElementsByClassName("invisible").length;
+  totals.invisible = invisibleCount
 
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
